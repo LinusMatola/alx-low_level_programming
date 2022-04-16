c7f48236bd6cdf43bb47b9ece3bae040d5619467
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-04-16 03:08:05 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,28 +4,26 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
- int n;
-  
+int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-  printf("%i is positive\n", n);
+printf("%i is positive\n", n);
 }
 else if (n == 0)
 {
-  printf("%i is zero\n", n);
+printf("%i is zero\n", n);
 }
 else if (n < 0)
 {
-  printf("%i is negative\n", n);
+printf("%i is negative\n", n);
 }
-
 return (0);
 }
