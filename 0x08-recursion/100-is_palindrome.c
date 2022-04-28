55611c4504c6e8dfa994277fa375967a178aca1e
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-04-29 01:05:12 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,48 +1,50 @@
 #include "main.h"
-int lengthc(char *s);
-int palindrome(char str[], int st, int end);
+
 /**
-* is_palindrome - Entry Point
-* @s: input
-* Return: 0
+* _strlen_recursion - size
+* @s: pointer to string params
+* Return: recursion
 */
-int is_palindrome(char *s)
+
+int _strlen_recursion(char *s)
 {
-	int len;
-
-	len = lengthc(s);
-
-	if (len == 0)
-		return (1);
-	return (palindrome(s, 0, len - 1));
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
 }
 
 /**
- * lengthc - finds the length count
- * @s: input
- * Return: length size
- */
-int lengthc(char *s)
+* p1 - palindrome
+* @s: pointer to string
+* @l: position
+* Return: boolena
+*/
+
+int p1(char *s, int l)
 {
-	if (*s != '\0')
-		return (1 + lengthc(s + 1));
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
 	return (0);
 }
 
 /**
-* palindrome - checks if start and end of string matches
-* @str: string
-* @st: start of string 0
-* @end: end of string from is_palindrome, from lengthc
-* Return: if str is a palindrome
+* is_palindrome - palindrome
+* @s: pointer to string
+* Return: recursion
 */
-int palindrome(char str[], int st, int end)
+
+int is_palindrome(char *s)
 {
-	if (st >= end)
-		return (1);
-	if (str[st] != str[end])
-		return (0);
-	if (st <= end || st < end + 1)
-		return (palindrome(str, st + 1, end - 1));
-	return (1);
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
 }
