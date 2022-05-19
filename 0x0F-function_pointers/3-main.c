35a053c3578ea2d85b4a23a5225fead186b43ccf
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-05-19 13:58:05 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
- */
+* main - get_op_func has operators correlated with
+* func signs and funcs from op_func
+* if not 4 arguments, return Error & exit 98
+* if op is null, return Error & exit 99
+* if div or mod 0, return Error & exit 100
+* run calc, input one, operator, input two = pointer res to get_op
+* @argc: arguments
+* @argv: double pointer to arguments
+* Return: 0
+*/
 int main(int argc, char *argv[])
 {
 	int one, two, ans;
