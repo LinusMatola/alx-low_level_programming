c220e65d9b1c89d7f1ff4a02bb5d79e33fb1e5f7
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-07-11 11:44:11 +0300

    update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /* Libraries */
 #include <stdio.h>
@@ -27,4 +27,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
